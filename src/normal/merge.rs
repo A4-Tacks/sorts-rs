5e5b78549dee78ea793b9fc8f1519f325b3fbed9
A4-Tacks <wdsjxhno1001@163.com>
2024-09-04 04:17:58 +0800

    修复归并算法的逆向归并实现错误

--- src/normal/merge.rs
+++ src/normal/merge.rs
@@ -30,6 +30,10 @@ use crate::{cmp, i};
 /// let mut arr = [1, 3, 5, 7, 8, 2, 4, 6];
 /// merge(&mut arr, 5, &mut buf, i32::lt);
 /// assert_eq!(arr, [1, 2, 3, 4, 5, 6, 7, 8]);
+///
+/// let mut arr = [4, 5, 6, 7, 8, 1, 2, 3];
+/// merge(&mut arr, 5, &mut buf, i32::lt);
+/// assert_eq!(arr, [1, 2, 3, 4, 5, 6, 7, 8]);
 /// ```
 pub fn merge<T, F>(
     arr: &mut [T],
@@ -76,7 +80,7 @@ where F: FnMut(&T, &T) -> bool,
             }
         }
         for ele in i {
-            arr[i!(--j)] = ele;
+            arr[i!(--k)] = ele;
         }
     }
 }
